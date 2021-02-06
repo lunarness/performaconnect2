5cc6a69f2aac53f1ee7829df3e4f159bf021190b
Vanessa Klevenz <vanessaklevenz@gmail.com>
2021-02-07 10:20:20 +1100

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
@@ -60,5 +60,3 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
-
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
